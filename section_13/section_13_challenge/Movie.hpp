c54701c78b89e851f992ca412e73cfbb174c7010
UNAKNOWNYMOUS <UNAKNOWNYMOUS@protonmail.com>
2026-02-07 00:36:24 -0500

    Almost done with section going to take break

---
+++ section_13/section_13_challenge/Movie.hpp
@@ -0,0 +1,13 @@
+#ifndef _MOVIE_HPP_
+#define _MOVIE_HPP_
+
+#include <string>
+
+class Movie {
+public:
+private:
+  std::string name;
+  std::string rating;
+  size_t watched;
+};
+#endif
---
+++ section_13/section_13_challenge/Movies.hpp
@@ -0,0 +1,11 @@
+#ifndef _MOVIES_HPP_
+#define _MOVIES_HPP_
+
+#include "Movie.hpp"
+#include <vector>
+
+class Movies {
+public:
+movies.private : std::vector<Movie> list;
+};
+#endif
